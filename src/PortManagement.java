6a2f09b5f11d7fc127e83edce24f4c424d6ed907
JorgeGarciaMarin <garciamarinjorge8@gmail.com>
2023-05-03 12:32:40 +0200

    Commited

--- src/PortManagement.java
+++ src/PortManagement.java
@@ -2,12 +2,10 @@ import Exceptions.*;
 
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.IOException;
-public class PortManagement extends JFrame {
+public class PortManagement extends JFrame  {
     private Hub hub3=new Hub();
-    private Hub h=new Hub();
     private Hub hub2=new Hub();
     private Hub hub1=new Hub();
     private JTextArea taDescription;
@@ -33,6 +31,8 @@ public class PortManagement extends JFrame {
     public JComboBox comboBox1;
     private JComboBox comboBox2;
     private JTextField hubNumber;
+    private JButton avgWeightCountryButton;
+    private JTextField textFieldAvg;
     public boolean shown=false;
 
     public PortManagement() {
@@ -172,6 +172,8 @@ public class PortManagement extends JFrame {
             }
         });
     }
+
+
 
     public static void main(String[] args) throws IOException {
         PortManagement myFrame=new PortManagement();
